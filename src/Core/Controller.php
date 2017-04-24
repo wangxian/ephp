eb2705c42291631a8cbd4a536f58950784a13c98
wangxian <xian366@126.com>
2017-04-24 17:10:25 +0800

    更新：父控制器，增加bool isAjax()方法

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -66,4 +66,18 @@ class Controller
     {
         throw new \ePHP\Exception\ExitException();
     }
+
+    /**
+     * Check if the request is from Ajax
+     *
+     * @return bool
+     */
+    protected function isAjax()
+    {
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && $_SERVER['HTTP_X_REQUESTED_WITH'] == "XMLHttpRequest" ) {
+            return true;
+        }
+        return false;
+    }
 }
