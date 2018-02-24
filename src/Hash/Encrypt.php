0cb93409b890da4e7a52970546ffd76a82e4fbb4
wangxian <xian366@126.com>
2018-02-24 17:04:30 +0800

    修复：Encrypt::edcode 问题

--- src/Hash/Encrypt.php
+++ src/Hash/Encrypt.php
@@ -62,20 +62,23 @@ class Encrypt
     {
         // ENCODE
         $key_length    = strlen($key);
-        $string        = $operation == 'DECODE' ? Func::safe_b64decode($string) : substr(md5($string . $key), 0, 8) . $string;
+        $string        = $operation == 'DECODE' ? \ePHP\Misc\Func::safe_b64decode($string) : substr(md5($string . $key), 0, 8) . $string;
         $string_length = strlen($string);
-        $rndkey        = $box        = array();
+        $rndkey        = $box = array();
         $result        = '';
+
         for ($i = 0; $i <= 255; $i++) {
             $rndkey[$i] = ord($key[$i % $key_length]);
             $box[$i]    = $i;
         }
+
         for ($j = $i = 0; $i < 256; $i++) {
             $j       = ($j + $box[$i] + $rndkey[$i]) % 256;
             $tmp     = $box[$i];
             $box[$i] = $box[$j];
             $box[$j] = $tmp;
         }
+
         for ($a = $j = $i = 0; $i < $string_length; $i++) {
             $a       = ($a + 1) % 256;
             $j       = ($j + $box[$a]) % 256;
@@ -93,7 +96,7 @@ class Encrypt
                 return '';
             }
         } else {
-            return str_replace('=', '', Func::safe_b64encode($result));
+            return \ePHP\Misc\Func::safe_b64encode($result);
         }
     }
 
